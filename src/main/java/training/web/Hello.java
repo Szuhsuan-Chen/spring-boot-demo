e0f94e6723c2a7754f9612530edbe97bb01aa33d
Susan Chen <V003322@aver.com>
2026-03-10 16:19:20 +0800

    feat: set up spring boot web app with root endpoint

--- src/main/java/training/web/Hello.java
+++ src/main/java/training/web/Hello.java
@@ -1,7 +1,23 @@
 package training.web;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+//加入首頁
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.GetMapping;
+
+//標示成Spring Boot應用程式
+@SpringBootApplication
+@RestController
 public class Hello {
     public static void main(String[] args) {
-        System.out.println("Hello Spring Boot!");
+        //啟動網站應用 http://127.0.0.1:8080
+        SpringApplication.run(Hello.class, args);
+    }
+
+    //首頁
+    @GetMapping("/")
+    public String index() {
+        return "Hello, Spring Boot!";
     }
 }
