1f7af320dbd1bab89c6dfda9b3ffe583998ab170
Susan Chen <V003322@aver.com>
2026-03-17 11:25:02 +0800

    feat: add REST endpoints returning array, list, map, and object

--- src/main/java/training/web/Hello.java
+++ src/main/java/training/web/Hello.java
@@ -1,4 +1,6 @@
 package training.web;
+import java.util.List;
+import java.util.Map;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -23,10 +25,42 @@ public class Hello {
         return "Hello, Spring Boot!";
     }
 
-    //處理來自路徑 /test 的請求
-    @GetMapping("/test")
-    public String test() {
-        return "Hello, Routing Test!";
+    //處理來自路徑 /getIntArray 的請求
+    @GetMapping("/getIntArray")
+    public int[] getIntArray() {
+        int[] data = {10, 1, 3, 4};
+        return data;
+    }
+
+    @GetMapping("/getIntList")
+    public List<Integer> getIntList() {
+        List<Integer> data = List.of(3, 10, 2, 1);
+        return data;
+    }
+
+    @GetMapping("/getCoordinates")
+    public Map getCoordinates() {
+        Map data = Map.of("x", 10, "y", 20, "z", 30);
+        return data;
+    }
+
+    @GetMapping("/getPoint")
+    public Point getPoint() {
+        Point p1 = new Point(10, 20);
+        return p1;
+    }
+
+    @GetMapping("/getPointList")
+    public List getPointList() {
+        Point p1 = new Point(10, 20);
+        Point p2 = new Point(30, 40);
+        return List.of(p1, p2);
+    }
+
+    @GetMapping("/getStudentGrades")
+    public Map getStudentGrades() {
+        int[] data = {60, 70 , 80, 90};
+        return Map.of("name", "Student Grade", "data", data);
     }
 
     //處理來自路徑 /echo?name=名字 的請求
@@ -37,9 +71,9 @@ public class Hello {
 
     //處理來自路徑 /add?n1=整數&n2=整數 的請求
     @GetMapping("/add")
-    public String add(@RequestParam("n1") int n1, @RequestParam("n2") int n2) {
+    public Map add(@RequestParam("n1") int n1, @RequestParam("n2") int n2) {
         int result = n1 + n2;
-        return "The sum of " + n1 + " and " + n2 + " is " + result + ".";
+        return Map.of("result", result);
     }
 
     //處理來自路徑 /test/任意的文字 的請求
